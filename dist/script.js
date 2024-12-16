2bcea453045c52ab4e990268d581010109f60148
muleman69 <129651545+muleman69@users.noreply.github.com>
2024-12-16 14:19:57 -0800

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('scroll', () => {
+  const scrolled = window.pageYOffset;
+  const heroImage = document.querySelector('.hero-content img');
+  if (heroImage) {
+    heroImage.style.transform = `perspective(1000px) rotateY(-15deg) translateY(${scrolled * 0.1}px)`;
+  }
+});
